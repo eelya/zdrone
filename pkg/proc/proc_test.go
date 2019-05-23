020ad6db1609021c36a76a1a313edad1f6ffcad9
Ilya Z <ilyamz@gmail.com>
2019-05-23 13:46:22 -0700

    remove bad test

--- pkg/proc/proc_test.go
+++ pkg/proc/proc_test.go
@@ -1,7 +1,6 @@
 package proc
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,11 +22,4 @@ func TestMul2(t *testing.T) {
 	assert := assert.New(t)
 	p := NewProc(11)
 	assert.Equal(121, p.Mul(11))
-}
-
-func TestEnv(t *testing.T) {
-	assert := assert.New(t)
-	p, ok := os.LookupEnv("FOO")
-	assert.True(ok, "FOO must be set")
-	assert.Equal(p, "BAR")
 }
