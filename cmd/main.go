1aff4a86b30cd6fae58285d4562e6e1ccc16464e
Ilya Z <ilyamz@gmail.com>
2019-05-24 17:18:12 -0700

    add counter'

--- cmd/main.go
+++ cmd/main.go
@@ -11,12 +11,15 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+var count = 0
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Debugf("Handle %s request to %s from %s", r.Method, r.URL, r.RemoteAddr)
 	r.ParseForm()
 	f, _ := strconv.Atoi(r.Form.Get("x"))
 	p := proc.NewProc(19)
-	fmt.Fprintf(w, "hey %v %v", p.Add(f), p.Mul(f))
+	fmt.Fprintf(w, "hey %v %v, count is %d", p.Add(f), p.Mul(f), count)
+	count++
 }
 
 func main() {
